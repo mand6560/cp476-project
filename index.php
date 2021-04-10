500de398f711c384c340af2efb2cda8ae421138d
Hayden Jeanson <haydenjeanson@gmail.com>
2021-04-10 16:19:16 -0400

    added custom seach to index

--- index.php
+++ index.php
@@ -12,14 +12,23 @@
             <hr />
         </div>
 
+        <h3>Quick Search</h3>
         <form action="search.php" method="post">
-        <div class="search">
-            <input class="button" type="submit" name="search_box" value="Math"/>
-            <input class="button" type="submit" name="search_box" value="Science"/>
-            <input class="button" type="submit" name="search_box" value="English"/>
-            <input class="button" type="submit" name="search_box" value="History"/>
-        </div>
-    </form>
+            <div class="search">
+                <input class="button" type="submit" name="search_box" value="Math"/>
+                <input class="button" type="submit" name="search_box" value="Science"/>
+                <input class="button" type="submit" name="search_box" value="English"/>
+                <input class="button" type="submit" name="search_box" value="History"/>
+            </div>
+        </form>
+        
+        <h3>Custom Search</h3>
+        <form action="search.php" method="post">
+            <div class="search">
+                <input type = "Text" placeholder = "Search for textbooks" value = "" name="search_box"/>
+                <input class="button buttonSearch" type="submit" value="Search"/>
+            </div>
+        </form>
 
         <hr />
         <footer>
