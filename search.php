2082c6f1a439a36734e2b4e45d9a61d9f9318261
Hayden Jeanson <haydenjeanson@gmail.com>
2021-04-10 13:00:39 -0400

    integrated dbQuery

--- search.php
+++ search.php
@@ -42,36 +42,21 @@
         </tr>
 
         <?php
-        include_once 'MDBManager.php';
-        
-        $dbname = 'studentsaver';
-        $collection = 'Products';
+        include_once 'dbQuery.php';        
 
-        $dbm = new MDBManager();
-        $conn = $dbm->getConnection();
-
+        $resultsFound = FALSE;
         
-        //"title" => $name
-        
-        $filter = [];
-        $option = [];
-        $read = new MongoDB\Driver\Query($filter, $option);
-        $all = $conn->executeQuery("$dbname.$collection", $read);
-        $foo = FALSE;
-        
-        if(!empty($_POST['search_box'])){
+        if (!empty($_POST['search_box'])) {
             $name = $_POST['search_box'];
-        
-            foreach ($all as $textbook) {
-                
 
-                if(strpos(strtolower($textbook->title), strtolower($name)) !== False || strpos(strtolower($textbook->author), strtolower($name)) !== False || strpos(strtolower($textbook->isbn), strtolower($name)) !== False || strpos(strtolower($textbook->price), strtolower($name)) !== False || strpos(strtolower($textbook->stock), strtolower($name)) !== False || strpos(strtolower($textbook->subject), strtolower($name)) !== False || strpos(strtolower($textbook->description), strtolower($name)) !== False){
-                    echo nl2br('<tr><td>' .  $textbook->title . '</td><td>' . $textbook->author . '</td><td>' . $textbook->isbn . '</td><td>' . "$" . $textbook->price . '</td><td>' . $textbook->stock.'</td><td>' . $textbook->subject.'</td><td>' . $textbook->description."</td></tr>");
-                    $foo = TRUE;
-                } 
+            $textbooks = (New DbQuery())->getResults($name);
+        
+            foreach ($textbooks as $textbook) {
+                echo nl2br('<tr><td>' .  $textbook->title . '</td><td>' . $textbook->author . '</td><td>' . $textbook->isbn . '</td><td>' . "$" . $textbook->price . '</td><td>' . $textbook->stock.'</td><td>' . $textbook->subject.'</td><td>' . $textbook->description."</td></tr>");
+                $resultsFound = TRUE;
             }
         }
-        if($foo == TRUE){
+        if ($resultsFound) {
             print("Results Found:");
         } else {
             print("No Results Found");
