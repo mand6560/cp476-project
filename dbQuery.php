8ac1ab5d8e93b9dc0d20b6a03fe63d60e755f53c
Hayden Jeanson <haydenjeanson@gmail.com>
2021-04-10 12:56:59 -0400

    code cleanup

--- dbQuery.php
+++ dbQuery.php
@@ -36,8 +36,8 @@
 include_once 'MDBManager.php';
 
 class DbQuery {
-    const DB_NAME = 'studentsaver';
-    const PRODUCTS_COLLECTION = 'Products';
+    private const DB_NAME = 'studentsaver';
+    private const PRODUCTS_COLLECTION = 'Products';
 
     private function makeConnection() {
         $dbm = new MDBManager();
@@ -48,17 +48,21 @@ class DbQuery {
 
     function getResults($search_query) {
         $conn = $this->makeConnection();
-
-        $filter = [];
-        $option = [];
-        $read = new MongoDB\Driver\Query($filter, $option);
+        $read = new MongoDB\Driver\Query([]);
         $all = $conn->executeQuery(self::DB_NAME . "." . self::PRODUCTS_COLLECTION, $read);
 
         $textbooks = [];
         
         if (!empty($search_query)) {
             foreach ($all as $textbook) {
-                if(strpos(strtolower($textbook->book_id), strtolower($search_query)) !== False || strpos(strtolower($textbook->title), strtolower($search_query)) !== False || strpos(strtolower($textbook->author), strtolower($search_query)) !== False || strpos(strtolower($textbook->isbn), strtolower($search_query)) !== False || strpos(strtolower($textbook->price), strtolower($search_query)) !== False || strpos(strtolower($textbook->stock), strtolower($search_query)) !== False || strpos(strtolower($textbook->subject), strtolower($search_query)) !== False || strpos(strtolower($textbook->description), strtolower($search_query)) !== False){
+                if(strpos(strtolower($textbook->book_id), strtolower($search_query)) !== False ||
+                 strpos(strtolower($textbook->title), strtolower($search_query)) !== False ||
+                 strpos(strtolower($textbook->author), strtolower($search_query)) !== False || 
+                 strpos(strtolower($textbook->isbn), strtolower($search_query)) !== False || 
+                 strpos(strtolower($textbook->price), strtolower($search_query)) !== False || 
+                 strpos(strtolower($textbook->stock), strtolower($search_query)) !== False || 
+                 strpos(strtolower($textbook->subject), strtolower($search_query)) !== False || 
+                 strpos(strtolower($textbook->description), strtolower($search_query)) !== False){
                     $textbooks[] = $textbook;
                 } 
             }
