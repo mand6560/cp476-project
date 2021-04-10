1974e54a81a9c917302061c4fd3465ae8a224d1c
Hayden Jeanson <haydenjeanson@gmail.com>
2021-04-10 12:53:47 -0400

    getResults method returns arrayt of textbooks matching query

--- dbQuery.php
+++ dbQuery.php
@@ -1,4 +1,4 @@
-class dbQuery {
+<!-- class dbQuery {
 
     private function makeConnection() {
         $dbname = 'cp476';
@@ -31,4 +31,41 @@ class dbQuery {
     private function lookforthis(this) {
         return result
     }
+} -->
+<?php
+include_once 'MDBManager.php';
+
+class DbQuery {
+    const DB_NAME = 'studentsaver';
+    const PRODUCTS_COLLECTION = 'Products';
+
+    private function makeConnection() {
+        $dbm = new MDBManager();
+        $conn = $dbm->getConnection();
+
+        return $conn;
+    }
+
+    function getResults($search_query) {
+        $conn = $this->makeConnection();
+
+        $filter = [];
+        $option = [];
+        $read = new MongoDB\Driver\Query($filter, $option);
+        $all = $conn->executeQuery(self::DB_NAME . "." . self::PRODUCTS_COLLECTION, $read);
+
+        $textbooks = [];
+        
+        if (!empty($search_query)) {
+            foreach ($all as $textbook) {
+                if(strpos(strtolower($textbook->book_id), strtolower($search_query)) !== False || strpos(strtolower($textbook->title), strtolower($search_query)) !== False || strpos(strtolower($textbook->author), strtolower($search_query)) !== False || strpos(strtolower($textbook->isbn), strtolower($search_query)) !== False || strpos(strtolower($textbook->price), strtolower($search_query)) !== False || strpos(strtolower($textbook->stock), strtolower($search_query)) !== False || strpos(strtolower($textbook->subject), strtolower($search_query)) !== False || strpos(strtolower($textbook->description), strtolower($search_query)) !== False){
+                    $textbooks[] = $textbook;
+                } 
+            }
+        }
+
+        return $textbooks;
+    }
 }
+
+?>
