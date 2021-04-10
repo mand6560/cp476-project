4ba822b66000da2965caf72e64ac4dc793239a4e
Hayden Jeanson <haydenjeanson@gmail.com>
2021-04-10 15:18:09 -0400

    created index.php with basic html structure

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>StudentSaver</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+        <link rel="stylesheet" href="checkout.css">
+    </head>
+
+    <body>
+        <div class="header">
+            <h1>StudentSaver</h1>
+            <hr />
+        </div>
+
+        
+        <hr />
+        <footer>
+            <p>COPYRIGHT &#169; 2021 StudentSaver</p>
+            <p>All rights reserved.</p>
+        </footer>
+    </body>
+</html>
